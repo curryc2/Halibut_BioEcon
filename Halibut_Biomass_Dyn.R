44984eaf2c7e368fae0020a61ae537cecbf503ba
Curry Cunningham <cunninghamcurry@gmail.com>
2017-06-07 11:46:24 -0800

    Updated file names and data import

--- Halibut_Biomass_Dyn.R
+++ Halibut_Biomass_Dyn.R
@@ -20,11 +20,31 @@
 #  3) Successfully setup with GitHub for cross-platform use
 #  4) Population viability analysis?
 #       Only comfortable 
-
+#  5) From 2016 RARA: Age-0 recruitment Coastwide ~55 million, Areas as fleets ~ 25 million
+#  6) From 2013 Presentation SSB0= 
+#     a) Coastwide = 709 million lbs.
+#     b) Areas 2B/2C/3A (SE/BC) = 489 million lbs.
+#  7) Finding ro:
+#     a) Set Fmort to zero
+#     b) Find ro that results in equilibrium SSB ~ 709 million
+#     c) This happens to be ro = 2.5e6
+#====================================================
+# If Recruitment in lbs:
+#  8a) In unfished condition w/ ro = 2.5e6
+#     Starting biomass at 1500 (million lbs) leads to stability.
+#
+# If Recruitment in fish:
+#  8b) In unfished condition w/ ro = 3e7
+#     Starting biomass at 1600 (million lbs) leads to stability.
+#
+# Fishing Mortality rates that bring population into 
+#====================================================
 #Ricker
 # R <- S*exp(1.25*log(5*h)*(1-(S/Seq)))
 #Beverton-Holt
 # R <- S/(1-((5*h-1)/(4*h))*(1-(S/(Seq))))
+
+#Fmort Starts: 0.075, 0.05, 0.01, 0.01
 
 #*************************************************************************************************
 require(ggplot2)
@@ -36,14 +56,9 @@ require(parallel)
 require(reshape2)
 require(xlsx)
 require(corrplot)
+require(R.utils)
 
 #Working Directory
-if(.Platform$OS.type=='unix') { #Laptop
-  wd <- '/Users/curryc2/Documents/2016/Reimer Halibut/Halibut_BioEcon'
-}else { #Desktop
-  wd <- "//nmfs.local/AKC-ABL/Users/curry.cunningham/My Documents/Projects/Halibut MSE/Halibut_BioEcon"  
-}
-setwd(wd)
 
 #Sources
 source('R/Halibut_Plot_Fxns.R')
@@ -51,19 +66,24 @@ source('R/ricker-recruit.R')
 source('R/beverton-holt-recruit.R')
 source('R/get-fished-survivorship.R')
 
+source('R/fisheryFootprint_plus.R') #This is an updated version of Steve's functions
+source('R/read_update_params.R')
+
 #PRELIMINARY PLOTS
 # plot.landings(dpi=600)
 
 
 
-#READ IN DATA from excel input file
-load('data/halibut.rda')
-source('R/fisheryFootprint_plus.R') #This is an updated version of Steve's functions
+# #READ IN DATA from excel input file
+# load('data/halibut.rda')
+
+
 
 #=============================================================
 ##### CONTROL SECTION #####
 do.init.plots <- FALSE
 
+halibut <- read_update_params(do.init.plots=TRUE)
 
 #=============================================================
 #Adjust halibut object values based on inputs from spreadsheet
@@ -125,12 +145,14 @@ if(do.init.plots==TRUE) { plot.growth_allometry(ageSchedules=halibut, dpi=500) }
 
 if(do.init.plots==TRUE) { plot.selectivity(selectivity=halibut, dpi=500, pt.blk=FALSE) }
 
+
+
 #=========================================
 #Extract variables
 n.age  <- halibut$theta$A
 n.gear <- dim(halibut$MP)[1]
 n.sex  <- halibut$theta$H
-va    <- as.array(halibut$selex) #Overall selectivity
+va <- as.array(halibut$selex) #Overall selectivity
 
 gears <- as.vector(halibut$MP$sector)
 
@@ -140,7 +162,6 @@ probRetain <- as.array(halibut$probRetain)
 
 wa <- halibut$ageSc$wa #Weight @ age
 fa <- halibut$ageSc$fa #Female spawning biomass @ age
-
 
 #Age Schedule stuff
 mx <- halibut$ageSc$mx  #Natural mortality @ age
@@ -214,18 +235,19 @@ for(a in 1:n.age) {
 }
 #Multiply by spawning sbpr @ age
 # init.prop <- init.prop
-init.prop <- init.prop
-for(s in 1:n.sex) {
-  init.prop[s,] <- init.prop[s,]/sum(init.prop[s,])
-}
+init.prop <- init.prop * wa
+# for(h in 1:n.sex) {
+#   init.prop[h,] <- init.prop[h,]/sum(init.prop[h,])
+# }
+
+init.prop <- init.prop/sum(init.prop)
 
 # lst <- melt(init.prop)
 # names(lst) <- c('Sex','Age','Prop')
 # ggplot(lst, aes(x=Age, y=Prop, color=Sex))+ geom_line() + geom_point(pch=21, fill='black') 
 
-
 #Initial Biomass
-B[,1,] <- Bstart*1e6 * init.prop
+B[,1,] <- Bstart*1e6 * (init.prop)
 
 #Initial Numbers of Individuals
 N[,1,] <- B[,1,] /wa
@@ -260,8 +282,8 @@ for(y in 2:n.year) {
   for(a in 1:n.age) {
     #Update Numbers and Biomass Matrix
     if(a==1) { #Age-1
-      B[,y,a] <- rec[,y-1]
-      N[,y,a] <- rec[,y-1]/wa[,a]
+      N[,y,a] <- rec[,y-1]
+      B[,y,a] <- rec[,y-1]*wa[,a]
     }else {
       h <- 1
       for(h in 1:n.sex) {
@@ -274,8 +296,10 @@ for(y in 2:n.year) {
         mort[h,y-1,a-1] <- 1-surv[h,y-1,a-1]
         
         #Update
-        B[h,y,a] <- B[h,y-1,a-1]*surv[h,y-1,a-1]
+        
         N[h,y,a] <- N[h,y-1,a-1]*surv[h,y-1,a-1]
+        # B[h,y,a] <- B[h,y-1,a-1]*surv[h,y-1,a-1]
+        B[h,y,a] <- N[h,y,a]*wa[h,a]
         #Total Catch
         C.n[h,y-1,a-1] <- N[h,y-1,a-1] * (F.a[h,y-1,a-1]/Z.a[h,y-1,a-1]) * (1-exp(-1*Z.a[h,y-1,a-1])) #Catch in number of halibut
         C.b[h,y-1,a-1] <- C.n[h,y-1,a-1] * wa[h,a-1]
@@ -284,7 +308,10 @@ for(y in 2:n.year) {
         for(g in 1:n.gear) {
           temp.F <- fmort[y-1,g]*va[h,a-1,g]
           temp.Z <- temp.F + mx[h,a-1]
-          harvest.n[h,y-1,a-1,g] <- N[h,y-1,a-1] * (F.a[h,y-1,a-1]/Z.a[h,y-1,a-1]) * (1-exp(-1*Z.a[h,y-1,a-1]))
+          # harvest.n[h,y-1,a-1,g] <- N[h,y-1,a-1] * (F.a[h,y-1,a-1]/Z.a[h,y-1,a-1]) * (1-exp(-1*Z.a[h,y-1,a-1]))
+          harvest.n[h,y-1,a-1,g] <- N[h,y-1,a-1] * (temp.F/temp.Z) * (1-exp(-1*temp.Z))
+          harvest.n[h,y-1,a-1,g] <- N[h,y-1,a-1] * (va[h,a-1,g] * (1-exp(-1*(mx[h,a-1]*Z.a[h,y-1,a-1])))) /(Z.a[h,y-1,a-1])
+          
           harvest.b[h,y-1,a-1,g] <- harvest.n[h,y-1,a-1,g] * wa[h,a-1]
         }#next gear
       }#next sex
@@ -302,8 +329,9 @@ for(y in 2:n.year) {
         mort[h,y-1,a] <- 1-surv[h,y-1,a]
         
         #Update
-        B[h,y,a] <- B[h,y,a] + B[h,y-1,a]*surv[h,y-1,a] #New Entrants (calculated above), plus existing plus group occupants.
-        N[h,y,a] <- N[h,y,a] + N[h,y-1,a]*surv[h,y-1,a]
+        N[h,y,a] <- N[h,y,a] + N[h,y-1,a]*surv[h,y-1,a] #New Entrants (calculated above), plus existing plus group occupants.
+        # B[h,y,a] <- B[h,y,a] + B[h,y-1,a]*surv[h,y-1,a] 
+        B[h,y,a] <- N[h,y,a] * wa[h,a]
         #Total Catch
         C.n[h,y-1,a] <- N[h,y-1,a] * (F.a[h,y-1,a]/Z.a[h,y-1,a]) * (1-exp(-1*Z.a[h,y-1,a])) #Catch in number of halibut
         C.b[h,y-1,a] <- C.n[h,y-1,a] * wa[h,a]
@@ -312,7 +340,7 @@ for(y in 2:n.year) {
         for(g in 1:n.gear) {
           temp.F <- fmort[y-1,g]*va[h,a,g]
           temp.Z <- temp.F + mx[h,a]
-          harvest.n[h,y-1,a,g] <- N[h,y-1,a] * (F.a[h,y-1,a]/Z.a[h,y-1,a]) * (1-exp(-1*Z.a[h,y-1,a]))
+          harvest.n[h,y-1,a,g] <- N[h,y-1,a] * (temp.F/temp.Z) * (1-exp(-1*temp.Z))
           harvest.b[h,y-1,a,g] <- harvest.n[h,y-1,a,g] * wa[h,a]
         }#next gear
       }#next sex
@@ -327,22 +355,45 @@ dim(C.n)
 dim(C.b)
 
 
+#Total Biomass plot
+#Unfished Biomass is coastise 709 million lbs
+list.B <- melt(B) 
+names(list.B) <- c('Sex','Year','Age','value')
+
+total.B <- list.B %>% group_by(Year, Age) %>% summarize(total=sum(value))
+
+gt <- ggplot(total.B, aes(x=Year, y=total/1e6, fill=Age, group=Age)) + 
+        theme_gray() + 
+        geom_area(alpha=0.75) +
+        scale_fill_gradient2(midpoint=plus.age/2, low='darkblue', mid='green', high='red') +
+        labs(y='Total Biomass (millions of lbs)') 
+gt
+
 #Plotting ssb
 list.ssb <- melt(ssb)
 names(list.ssb) <- c('Sex', 'Age', 'Year', 'value')
 
+
+SSB.eq <- 709
+SSB.2017 <- 212
+
 g <- ggplot(list.ssb[list.ssb$Sex=='Female',], aes(x=Year, y=value/1e6, fill=Age, group=Age)) +
        theme_gray() +
-       geom_area() + 
+       geom_area(alpha=0.75) + 
        scale_fill_gradient2(midpoint=plus.age/2, low='darkblue', mid='green', high='red') +
-       labs(y='Spawning Stock Biomass (millions of lbs)')   
+       labs(y='Spawning Stock Biomass (millions of lbs)') +
+       geom_hline(yintercept=SSB.eq, lty=2, col='red', show.legend=TRUE) +
+       geom_hline(yintercept=SSB.2017, lty=2, col='black', show.legend=TRUE)
 # scale_fill_brewer(palette="RdYlGn")
 g
 
+#
+
+
 # g <- list.ssb[list.ssb$Sex=='Female',] %>% group_by(Sex, Year) %>% summarize(total=sum(value, na.rm=TRUE)) %>% ggplot() + aes(x=Year, y=total, fill=Sex, group=Sex) + geom_line()
 
-list.ssb[list.ssb$Sex=='Female',] %>% group_by(Sex, Year) %>% 
-    summarize(total=sum(value, na.rm=TRUE), prop=(value/sum(value)))
+# list.ssb[list.ssb$Sex=='Female',] %>% group_by(Sex, Year) %>% 
+#     summarize(total=sum(value, na.rm=TRUE), prop=(value/sum(value)))
 
 #Plotting abundance
 dim(N)
@@ -365,7 +416,7 @@ g2
 #Plotting abundance proportions
 list.N.prop <- data.frame(list.N %>% group_by(Sex, Year) %>% mutate(prop=value/sum(value)))
 
-g3 <- ggplot(list.N.prop[list.N.prop$Year<10,], aes(x=Year, y=prop, fill=Age, group=Age)) +
+g3 <- ggplot(list.N.prop, aes(x=Year, y=prop, fill=Age, group=Age)) +
         theme_gray() +
         geom_area(alpha=0.75) +
         facet_wrap(~Sex, ncol=1) +
@@ -374,7 +425,7 @@ g3 <- ggplot(list.N.prop[list.N.prop$Year<10,], aes(x=Year, y=prop, fill=Age, gr
 g3
 
 list.b <- melt(B)
-names(list.b) <- c('Sex','Age','Year','value')
+names(list.b) <- c('Sex','Year','Age','value')
 
 list.B.prop <- data.frame(list.b %>% group_by(Sex, Year) %>% mutate(prop=value/sum(value)))
 
@@ -386,10 +437,49 @@ g4 <- ggplot(list.B.prop, aes(x=Year, y=prop, fill=Age, group=Age)) +
   labs(y='Proportion of Total Biomass (B)') 
 g4
 
+g5 <- ggplot(list.B.prop, aes(x=Year, y=value/1e6, fill=Age, group=Age)) +
+  theme_gray() +
+  geom_area(alpha=0.75) +
+  facet_wrap(~Sex, ncol=1) +
+  scale_fill_gradient2(midpoint=plus.age/2, low='darkblue', mid='green', high='red') +
+  labs(y='Total Biomass (millions of lbs)') 
+g5 
 
-g.rec <- ggplot()
 
 
+
+# g.rec <- ggplot()
+
+#2016
+# Commercial fishery landings= 25 milion lbs
+#Catch
+C.b.list <- melt(C.b)
+names(C.b.list) <- c('Sex','Year','Age','value')
+
+g.6 <- ggplot(C.b.list, aes(x=Year, y=value/1e6, fill=Age, group=Age)) +
+  theme_gray() +
+  geom_area(alpha=0.75) +
+  facet_wrap(~Sex, ncol=1) +
+  scale_fill_gradient2(midpoint=plus.age/2, low='darkblue', mid='green', high='red') +
+  labs(y='Total Catch (millions of lbs)') 
+g.6
+
+#Harvest by Sector
+harv.b.list <- melt(harvest.b)
+names(harv.b.list) <- c('Sex','Year','Age','Sector','value')
+
+g.7 <- ggplot(harv.b.list, aes(x=Year, y=value/1e6, fill=Age, group=Age)) +
+  theme_gray() +
+  geom_area(alpha=0.75) +
+  facet_grid(Sector~Sex) +
+  scale_fill_gradient2(midpoint=plus.age/2, low='darkblue', mid='green', high='red') +
+  labs(y='Total Catch (millions of lbs)') 
+
+g.7
+
+
+
+harv.b.total <- melt(apply())
 
 
 # Survivorship under fished conditions at fstar
---
+++ R/read-update-params.R
@@ -0,0 +1,72 @@
+#' Read in and update halibut life history parameters form .xlsx. 
+#'   Calculate size, mortality, and selectivity parameters at age, given sex and gear type. 
+#'
+#' @param do.init.plots Boolean for whether to generate figures describing 
+#'
+#' @return halibut a list including all available growth, mortality, selectivity information for the simulated stock. 
+#' @export
+#'
+read_update_params <- function(do.init.plots=FALSE) {
+  
+  load('data/halibut.rda')
+  #=============================================================
+  #Adjust halibut object values based on inputs from spreadsheet
+  #Growth
+  in.growth <- read.xlsx('Halibut Model Inputs.xlsx', sheetName='Growth')
+  halibut$theta$linf <- as.numeric(in.growth[in.growth$Par=='linf',(2:3)])
+  halibut$theta$vbk <- as.numeric(in.growth[in.growth$Par=='vbk',(2:3)])
+  halibut$theta$to <- as.numeric(in.growth[in.growth$Par=='to',(2:3)])
+  halibut$theta$a <- as.numeric(in.growth[in.growth$Par=='a',(2:3)])
+  halibut$theta$b <- as.numeric(in.growth[in.growth$Par=='b',(2:3)])
+  #Maturity
+  in.maturity <- read.xlsx('Halibut Model Inputs.xlsx', sheetName='Maturity')
+  halibut$theta$ahat <- as.numeric(in.maturity[in.maturity$Par=='ahat',(2:3)])
+  halibut$theta$ghat <- as.numeric(in.maturity[in.maturity$Par=='ghat',(2:3)])
+  #Mortality
+  in.mortality <- read.xlsx('Halibut Model Inputs.xlsx', sheetName='Mortality')
+  halibut$theta$m <- as.numeric(in.mortality[in.mortality$Par=='m',(2:3)])
+  halibut$theta$A <- max(in.mortality[in.mortality$Par=='A',2:3])
+  #Fishery Selectivity
+  in.selex <- read.xlsx2('Halibut Model Inputs.xlsx', sheetName='FisherySelectivity', stringsAsFactors=FALSE, header=TRUE)
+  halibut$MP$slx1 <- as.numeric(in.selex[in.selex$Par=='mu',c(2:5)])
+  halibut$MP$slx2 <- as.numeric(in.selex[in.selex$Par=='sigma',c(2:5)])
+  halibut$MP$slx3 <- as.numeric(in.selex[in.selex$Par=='gamma',c(2:5)])
+  halibut$MP$slx4 <- as.numeric(in.selex[in.selex$Par=='plus.age',c(2:5)])  #Plus group age for selectivity
+  halibut$MP$slim <- as.numeric(in.selex[in.selex$Par=='slim',c(2:5)])  #Minimum size for retention
+  halibut$MP$ulim <- as.numeric(in.selex[in.selex$Par=='ulim',c(2:5)])  #Maximum size for retention
+  halibut$MP$dmr <- as.numeric(in.selex[in.selex$Par=='dmr',c(2:5)])  #Discard Mortality Rate
+  # halibut$MP$pscLimit <- as.numeric(in.selex[in.selex$Par=='pscLimit',c(2:5)])  #Prohibited species catch limit
+  halibut$MP$pYPR <- as.numeric(in.selex[in.selex$Par=='pYPR',c(2:5)])
+  halibut$MP$pYPR <- as.numeric(in.selex[in.selex$Par=='pYPR',c(2:5)])
+  halibut$MP$pYPR <- as.numeric(in.selex[in.selex$Par=='pYPR',c(2:5)])
+  
+  #Recruitment Parameter Inputs
+  in.rec <- read.xlsx2('Halibut Model Inputs.xlsx', sheetName='Recruitment', stringsAsFactors=FALSE)
+  halibut$rec$steep <- as.numeric(in.rec$Value[in.rec$Par=='steep'])
+  halibut$rec$sigma_rec <- as.numeric(in.rec$Value[in.rec$Par=='sigma_rec'])
+  halibut$rec$ro  <- as.numeric(in.rec$Value[in.rec$Par=='ro'])
+  
+  #INPUT FISHING MORTALITY RATES
+  fmort <- read.xlsx('Halibut Model Inputs.xlsx', sheetName='Fmort')[,-1]
+  
+  #Input control parameters
+  in.control <- read.xlsx('Halibut Model Inputs.xlsx', sheetName='Control')
+  n.year <- in.control$Value[in.control$Par=='n.yrs'] #Number of years to simulate
+  years <- 1:n.year
+  Bstart <- in.control$Value[in.control$Par=='Bstart'] #Starting Biomass
+  
+  #=========================================
+  halibut <- getSelectivities(halibut)
+  #Determine Age Schedules
+  # ageSchedules <- getAgeSchedules(halibut)
+  #Plot Age Schedule
+  if(do.init.plots==TRUE) { plot.growth_allometry(ageSchedules=halibut, dpi=500) }
+  #
+  
+  #=========================================
+  #Determine Selectivities
+  # selectivity <- getSelectivities(halibut)
+  
+  if(do.init.plots==TRUE) { plot.selectivity(selectivity=halibut, dpi=500, pt.blk=FALSE) }
+  return(halibut)
+}
